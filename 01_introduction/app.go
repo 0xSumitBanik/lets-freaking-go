63c1adb0e5465c4adf565cc8ea0bb6a73d5239c1
Sumit Banik <sumitbanik.02@gmail.com>
2022-05-06 22:28:36 +0530

    [add] introduction module

---
+++ 01_introduction/app.go
@@ -0,0 +1,11 @@
+
+// The main package must have package name main 
+// and declare a function main that takes no arguments and returns no value.
+// Ref: https://go.dev/ref/spec#Program_execution
+
+package main
+
+func main() {
+	print("Let's freaking Go!")        // this function is present in builtin package
+	println("\nSure, let's do it!")    // this function is present in builtin package
+}
---
+++ 01_introduction/go.mod
@@ -0,0 +1,10 @@
+// This go.mod file was created using the command:
+// go mod init <module_name>
+
+// Go mod provides access to operations on modules.
+// For example, day-to-day adding, removing, upgrading,
+// and downgrading of dependencies should be done using 'go get'.
+
+module introduction
+
+go 1.17

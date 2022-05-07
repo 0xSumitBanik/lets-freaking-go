d30db7047399d255d74a88494a328c9102b4e8bb
Sumit Banik <sumitbanik.02@gmail.com>
2022-05-07 22:47:31 +0530

    [add] functions declarations and calls

---
+++ 06_Functions/app.go
@@ -0,0 +1,50 @@
+package main
+
+func add(x int, y int) int {
+	return x + y
+}
+
+// If the type portions of some successive parameters in a parameter declaration list are identical,
+// then these parameters could share the same type portion in the parameter declaration list.
+// 👆 Same goes for the return variable type.
+
+func addAndSub(x, y int) (s, d int) {
+	return x + y, x - y
+	// Named return:
+	// s = x + y
+	// d = x - y
+	// return
+}
+
+func main() {
+	var res int = add(1, 2)
+	addn, sub := addAndSub(1, 26)
+	println(res)
+	println(addn, sub)
+
+	// This anonymous function has no parameters
+	// but has two results.
+	x, y := func() (int, int) {
+		println("This function has no parameters.")
+		return 3, 4
+	}() // Call it. No arguments are needed.
+
+	// The following anonymous function have no results.
+
+	func(a, b int) {
+		// The following line prints: a*a + b*b = 25
+		println("a*a + b*b =", a*a+b*b)
+	}(x, y) // pass argument x and y to parameter a and b.
+
+	func(x int) {
+		// The parameter x shadows the outer x.
+		// The following line prints: x*x + y*y = 32
+		println("x*x + y*y =", x*x+y*y)
+	}(y) // pass argument y to parameter x.
+
+	func() {
+		// The following line prints: x*x + y*y = 25
+		println("x*x + y*y =", x*x+y*y)
+	}() // no arguments are needed.
+
+}

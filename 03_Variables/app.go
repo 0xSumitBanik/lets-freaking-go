751a5e1e1c276ae8ec9f84841277183f65fc4770
Sumit Banik <sumitbanik.02@gmail.com>
2022-05-07 16:20:32 +0530

    [add] different value literals

--- 03_Variables/app.go
+++ 03_Variables/app.go
@@ -30,4 +30,37 @@ func main() {
 	y:= "HI" // This is a short hand operator for var y string = "HI"
 
 	println(x,y)
+
+	// Integer Value Literals
+	// The following are the different types of integer value literals:
+	// 1. Decimal
+  var dd int = 28
+	// 2. Hexadecimal
+	var hh int = 0x1f
+	// 3. Octal
+	var oo int = 027 // prefix it with 0
+	// 4. Binary
+	var bb int = 0b1010
+
+	println(dd, hh, oo, bb)
+
+	// Floating Point Value Literals
+	// The following are the different types of floating point value literals:
+	// 1. Decimal
+	var dd1 float32 = 28.5
+	// 2. Hexadecimal
+	var hh1 float32 = 0x1f.p5
+	// 3. Octal
+	var oo1 float32 = 027.5 // prefix it with 0
+	println(dd1, hh1, oo1)
+	
+	// String Value literals
+	// The following are the different types of string value literals:
+	// 1. Double quoted string
+	var s1 string = "Hello\nWorld"
+	// 2. Raw Form String
+	var s2 string = `Hello
+	World`
+
+	println(s1, s2)
 }

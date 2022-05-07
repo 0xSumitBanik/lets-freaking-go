3dc48c3fda7eb1178e96ba4f1088a3eee9f99785
Sumit Banik <sumitbanik.02@gmail.com>
2022-05-07 22:49:11 +0530

    [note] about qualfied identifier

--- 02_importPackages/app.go
+++ 02_importPackages/app.go
@@ -11,6 +11,7 @@ import (
 // 👆 It is good style to use the factored import statement.
 
 func main(){
+	// The form aImportName.AnExportedIdentifier is called a qualified identifier
 	fmt.Println("Square root of 25 is",math.Sqrt(25))
 }
 

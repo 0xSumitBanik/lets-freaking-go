f537e17a7e34bc0ca187c968ba79153986b7d42f
Sumit Banik <sumitbanik.02@gmail.com>
2022-05-13 00:04:50 +0530

    [add] anonymous import

--- 02_importPackages/app.go
+++ 02_importPackages/app.go
@@ -3,9 +3,11 @@ package main
 // To use custom packages we use the import statement.
 // Ref: https://go.dev/ref/spec#Import_declarations
 
+// Syntax: import importname "path/to/package" (importname is optional)
 import (
 	"fmt"
 	"math"
+	_ "net/http/pprof" // anonymous import
 )
 
 // 👆 It is good style to use the factored import statement.

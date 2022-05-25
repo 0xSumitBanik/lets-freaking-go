2e6ceed3536479b153399785ca48094ff98689d2
Sumit Banik <sumitbanik.02@gmail.com>
2022-05-25 16:01:03 +0530

    [add] arrays and slices in go

---
+++ Go-Type-System/04_Arrays-Slices/app.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Arrays,Slices and Maps are first-class citizen container types in Go.
+// Each element in a container has an associated key.
+// For arrays and slices, it's an int value.
+// For maps, the key is of any comparable type.
+
+const Size = 32
+
+type Person struct {
+	name string
+	age  int
+}
+
+func main() {
+
+	/* Array types */
+
+	var array [Size]int
+	declaredArray := [5]int{1, 1: 2, 3, 4, 5} //we can also mention the index as key (optional)
+	// Element type is a struct type: Person
+	var personArray [4]Person
+	fmt.Printf("Type: %T %T\n", array, personArray)
+	fmt.Println(personArray, "\n\n Declared Array ->", declaredArray)
+	fmt.Printf("\n The length of array is %v`\n", len(array))
+
+	/* Slice types */
+
+	var slice []int
+	var personSlice []Person
+	fmt.Printf("Type: %T %T\n", slice, personSlice)
+	declaredSlice := []Person{{name: "name1", age: 1}, {name: "name2", age: 2}}
+	fmt.Println(personSlice, "\n\n Declared Slice ->", declaredSlice)
+	fmt.Printf("\n The length of slice is %v\n", len(declaredSlice))
+	fmt.Printf("\n The capactiy of declaredslice is %v\n", cap(declaredSlice))
+
+	// A slice is an abstraction that uses an array under the covers.
+	newSlice := make([]int, 3) // newSlice = [0,0,0]
+
+	for i := 0; i < 5; i++ {
+	  
+		// It will double the capacity for the next 3 elements and will create the array with the new capacity and memory address.
+		newSlice = append(newSlice, i) 
+		fmt.Printf("\n Capacity %v, Length %v, Slice Address: %p, %v", cap(newSlice), 
+		               len(newSlice), 
+		               newSlice, 
+									 newSlice)
+		/* Output:
+				Capacity 6, Length 4, Slice Address: 0xc00000c690, [0 0 0 0]
+				Capacity 6, Length 5, Slice Address: 0xc00000c690, [0 0 0 0 1]
+				Capacity 6, Length 6, Slice Address: 0xc00000c690, [0 0 0 0 1 2]
+				Capacity 12, Length 7, Slice Address: 0xc00001c240, [0 0 0 0 1 2 3]
+				Capacity 12, len 8, Slice Address: 0xc00001c240, [0 0 0 0 1 2 3 4]
+		*/
+	}
+
+	copiedSlice := make([]int, len(newSlice))
+	copy(copiedSlice,newSlice) // copy(destination,source)
+	fmt.Printf("\n Copied Slice %v , Address %p -> ",copiedSlice,copiedSlice)
+	
+	rangeSlice:=copiedSlice
+	fmt.Printf("\n Range Slice %v , Address %p -> ",rangeSlice,rangeSlice)
+	
+	// the ...s mean we want to let compilers deduce the lengths for the corresponding array values.
+	variableArray := [...]int{}
+	fmt.Printf("\n Variable Array %T",variableArray) // [0]int
+
+	
+
+}
